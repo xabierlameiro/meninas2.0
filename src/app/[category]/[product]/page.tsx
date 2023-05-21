bcc2704be7a059f2d826ac855723f838736d836b
Xabier Lameiro Cardama <xlameiro@hiberus.com>
2023-05-21 15:01:18 +0100

    Delete default files

---
+++ src/app/[category]/[product]/page.tsx
@@ -0,0 +1,5 @@
+
+
+export default function Page({params}: {params: {product: string}}) {
+    return <div>Page: {params.product}</div>
+}
---
+++ src/app/[category]/page.tsx
@@ -0,0 +1,5 @@
+
+
+export default function Page({params}: {params: {category: string}}) {
+    return <div>Page: {params.category}</div>
+}
