b3e22ca2caa5c9e31e0ef47365bcc6c4df9674c2
xabierlameiro <xabier.lameiro@gmail.com>
2023-06-01 17:44:48 +0100

    WIP

--- src/components/Menu/Button/index.tsx
+++ src/components/Menu/Button/index.tsx
@@ -50,6 +50,7 @@ const Button = ({ children }: any) => {
                 >
                     <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
                 </svg>
+
                 {children}
             </dialog>
         </div>
