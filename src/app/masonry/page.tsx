95ab31304594fdd93c984d5d131eca4bac28c8d8
xabierlameiro <xabier.lameiro@gmail.com>
2023-06-09 17:46:03 +0100

    Test

--- src/app/masonry/page.tsx
+++ src/app/masonry/page.tsx
@@ -22,18 +22,15 @@ const Home = async () => {
         >
             {data.map((product, index) => {
                 let width, height;
-                if (product.portada.width > 4000) {
-                    width = Math.floor(product.portada.width / 8);
-                    height = Math.floor(product.portada.height / 8);
-                } else if (product.portada.width > 3000) {
+                if (product.portada.width > 3000) {
+                    width = Math.floor(product.portada.width / 10);
+                    height = Math.floor(product.portada.height / 10);
+                } else if (product.portada.width > 2000) {
                     width = Math.floor(product.portada.width / 6);
                     height = Math.floor(product.portada.height / 6);
-                } else if (product.portada.width > 2000) {
-                    width = Math.floor(product.portada.width / 4);
-                    height = Math.floor(product.portada.height / 4);
                 } else if (product.portada.width > 1000) {
-                    width = Math.floor(product.portada.width / 2);
-                    height = Math.floor(product.portada.height / 2);
+                    width = Math.floor(product.portada.width / 3);
+                    height = Math.floor(product.portada.height / 3);
                 } else {
                     width = product.portada.width;
                     height = product.portada.height;
@@ -53,7 +50,7 @@ const Home = async () => {
                             alt={product.nombre}
                             width={width}
                             height={height}
-                            quality={100}
+                            quality={85}
                             layout="responsive"
                             style={{
                                 borderRadius: '15px',
