855b028ae74208189e1929163cdeba1d81d38e2e
xabierlameiro <xabier.lameiro@gmail.com>
2023-06-11 16:41:29 +0100

    New change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,6 @@ import GridContainer from '@components/Layout/GridContainer';
 import plp from '@queries/plp.graphql';
 import Masonry from '@components/Masonry';
 
-export const runtime = 'edge';
-
 const getProducts = async () => {
     const { data } = await fetchGraphQL(plp);
     return data.productoCollection.items as ContentfulProduct[];
