bb7f742fac638195cd3217c315d0729887a6e973
xabierlameiro <xabier.lameiro@gmail.com>
2023-06-14 09:59:10 +0100

    Edge

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@ import { fetchGraphQL } from '@helpers/contentful';
 import plp from '@queries/plp.graphql';
 import Masonry from '@components/Masonry';
 
+export const runtime = 'edge';
+
 const getProducts = async () => {
     const { data } = await fetchGraphQL(plp);
     return data.productoCollection.items as ContentfulProduct[];
