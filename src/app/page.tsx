c1a191c2e9d558333c51720cc0af3f6096b1aef9
xabierlameiro <xabier.lameiro@gmail.com>
2023-06-14 13:21:30 +0100

    More styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,6 @@ import { fetchGraphQL } from '@helpers/contentful';
 import plp from '@queries/plp.graphql';
 import Masonry from '@components/Masonry';
 
-export const runtime = 'edge';
-
 const getProducts = async () => {
     const { data } = await fetchGraphQL(plp);
     return data.productoCollection.items as ContentfulProduct[];
