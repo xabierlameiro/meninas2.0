e8379e61295be29789531529bbb99616c46acb27
xabierlameiro <xabier.lameiro@gmail.com>
2023-06-14 13:59:04 +0100

    Disable increment or decrement

--- src/components/Icon/index.tsx
+++ src/components/Icon/index.tsx
@@ -2,10 +2,12 @@
 import styles from './icon.module.css';
 
 const Icon = ({
+    tabIndex,
     onClick,
     children,
     width,
     height,
+    title,
     fill = 'none',
     scale = 1,
     strokeWidth = 1.5,
@@ -14,23 +16,25 @@ const Icon = ({
     viewBoxAspectRatio = false,
 }: Children & IconProps) => {
     return (
-        <svg
-            tabIndex={0}
-            xmlns="http://www.w3.org/2000/svg"
-            stroke="currentColor"
-            fill={fill}
-            transform={`scale(${scale})`}
-            viewBox={`${viewBoxAspectRatio ? '0 0 30 30' : '0 0 24 24'}`}
-            onClick={() => onClick?.()}
-            onKeyDown={(e) => e.key === 'Enter' && onClick?.()}
-            strokeWidth={strokeWidth}
-            width={width}
-            height={height}
-            className={`${styles.icon} ${className}`}
-            onAnimationEnd={onAnimationEnd}
-        >
-            {children}
-        </svg>
+        <div title={title}>
+            <svg
+                tabIndex={tabIndex ?? 0}
+                xmlns="http://www.w3.org/2000/svg"
+                stroke="currentColor"
+                fill={fill}
+                transform={`scale(${scale})`}
+                viewBox={`${viewBoxAspectRatio ? '0 0 30 30' : '0 0 24 24'}`}
+                onClick={() => onClick?.()}
+                onKeyDown={(e) => e.key === 'Enter' && onClick?.()}
+                strokeWidth={strokeWidth}
+                width={width}
+                height={height}
+                className={`${styles.icon} ${className}`}
+                onAnimationEnd={onAnimationEnd}
+            >
+                {children}
+            </svg>
+        </div>
     );
 };
 
